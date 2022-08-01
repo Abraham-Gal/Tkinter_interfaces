dfbfe8546a6cdc7c145e7e813eb48559ea874ba1
Eduardo Abraham Lopez Galvez <33144036@uaz.edu.mx>
2022-07-31 21:33:54 -0500

    Add dicctionary data
    Add dicctionary data

---
+++ Dicc_variables.py
@@ -0,0 +1,6 @@
+
+
+Colores = {'fondo':'#96E087','blanco':'#FFFFFF', 'fondo_n1':'#DDF9D7'}
+Agua = {'estado':"Sabe we"}
+
+#print (Agua['estado'])
--- Interfazv1.py
+++ Interfazv1.py
@@ -7,6 +7,7 @@ from Nodo2_interfaz import *
 from Conf_interfaz import *
 from Info_interfaz import *
 from Historial_interfaz import *
+from Dicc_variables import *
 
 
 class Inicio(tk.Tk):
@@ -14,19 +15,19 @@ class Inicio(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        self.config(width=1024, height=530,bg='#96E087')
+        self.config(width=1024, height=530,bg=Colores['fondo'])
         self.geometry("1024x530")
         self.title("Inicio")
         self.tk.call('wm','iconphoto',self._w,tk.PhotoImage(file='Interface/inicio_b.png'))  
 
-        #-----------------------------------  Fames         ----------------------
+        #-----------------------------------  Frames         ----------------------
         self.info_frame = Frame(self)
         self.info_frame.pack(side="left",pady="10",padx="10")
-        self.info_frame.config(bg="#FFFFFF",width="495", height="550",relief="flat", bd=3)
+        self.info_frame.config(bg=Colores['blanco'],width="495", height="550",relief="flat", bd=3)
 
         self.nodo1_frame = Frame(self)
         self.nodo1_frame.pack(anchor="se", pady="10", padx="10")
-        self.nodo1_frame.config(bg="#DDF9D7", width="510", height="247",relief="flat", bd=3)
+        self.nodo1_frame.config(bg=Colores['fondo_n1'], width="510", height="247",relief="flat", bd=3)
 
         self.nodo2_frame = Frame(self)
         self.nodo2_frame.pack(anchor="sw", pady="10", padx="10")
@@ -34,34 +35,50 @@ class Inicio(tk.Tk):
         #--------------------------------------------------------------------------
 
         #------------------------------------ Recursos   ---------------------------
-        self.png_agua=PhotoImage(file="interface/agua_e.png")
+        self.png_agua=PhotoImage(file="Interface/agua_e.png")
         #---------------------------------------------------------------------------
         
         #------------------------------------ Canvas obj --------------------------
-        #canvas = Canvas(self.info_frame)
-        #canvas.config(bg="#FFFFFF",width="495", height="550")
-        #canvas.pack(side="left",)
+        canvas = Canvas(self.info_frame)
+        canvas.config(bg="#FFFFFF",width="495", height="550")
+        canvas.pack(side="left")
         #--------------------------------------------------------------------------
 
         #----------------------------------- Objetos de info ----------------------
+        #========> Agua
         self.img_agua = Label(self.info_frame, image=self.png_agua, relief="flat", border=0)
-        self.img_agua.place(x=0, y=0)
+        self.img_agua.place(x=100, y=20)
+
+        self.label_liquido = Label(self.info_frame, text="Liquido", relief="flat", border=0, bg="#FFFFFF", font=("Verdana",24))
+        self.label_liquido.place(x=220, y=20)
+
+        self.label_estado = Label(self.info_frame, text="Estado:", relief="flat", border=0, bg="#FFFFFF", font=("Verdana",14))
+        self.label_estado.place(x=220, y=70)
+
+        self.label_DatoL = Label(self.info_frame, text=Agua['estado'], relief="flat", border=0, bg="#FFFFFF", font=("Verdana",14))
+        self.label_DatoL.place(x=295, y=70)
+
+        canvas.create_rectangle(30,120,465,115, fill="#96E087", outline="#96E087")
+
+        #========> Luz ambiental
+
+        
 
         self.boton_Config = ttk.Button(self.info_frame, text="Configuracion", command=self.abrir_Configuracion)
-        self.boton_Config.place(x=100, y=100)
+        self.boton_Config.place(x=100, y=300)
 
         self.boton_Info = ttk.Button(self.info_frame, text="Información", command=self.abrir_info)
-        self.boton_Info.place(x=100, y=150)
+        self.boton_Info.place(x=100, y=330)
 
         self.boton_Historial = ttk.Button(self.info_frame, text="Historial", command=self.abrir_Historial)
-        self.boton_Historial.place(x=100,y=200)
+        self.boton_Historial.place(x=100,y=360)
         #self.etiqueta_titulo1 = tk.Label(self.info_frame, text="Dispositivo de control inteligente para",bg="#FFFFFF", font=("Times", 20))
         #self.etiqueta_titulo1.place(x=33,y=10)
         
         #self.etiqueta_titulo2 = tk.Label(self.info_frame, text="microcultivos",bg="#FFFFFF", font=("Times", 20))
         #self.etiqueta_titulo2.place(x=33,y=60)
 
-        #canvas.create_line(30,100,465,100)
+        
         #-------------------------------------------------------------------------
         
         #----------------------------------- Objetos de nodo1 ----------------------
